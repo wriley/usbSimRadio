46b77b69a3385b64cd7cddcc95b1648bb42a9cb9
riley <riley@5fab1530-1bbb-4bbe-9b7e-7ebf187c0958>
2008-12-24 18:24:59 +0000

    untested changes for reading 4 encoders and 8 buttons on ATMega16

--- firmware/main.c
+++ firmware/main.c
@@ -24,186 +24,71 @@ at least be connected to INT0 as well.
 #include <avr/pgmspace.h>   /* required by usbdrv.h */
 #include "usbdrv.h"
 #include "oddebug.h"        /* This is also an example for using debug macros */
-
-#define PHASE_A		(PINB & 1<<PB1)
-#define PHASE_B		(PINB & 1<<PB2)
-#define	XTAL		12e6			// 12MHz
+#include "radio.h";
 
 /* ------------------------------------------------------------------------- */
 /* ----------------------------- USB interface ----------------------------- */
 /* ------------------------------------------------------------------------- */
 
-PROGMEM char usbHidReportDescriptor[22] = {    /* USB report descriptor */
-    0x06, 0x00, 0xff,              // USAGE_PAGE (Generic Desktop)
+PROGMEM char usbHidReportDescriptor[14] = {    /* USB report descriptor */
+    0x06, 0x00, 0xff,              // USAGE_PAGE (Vendor Defined Page 1)
     0x09, 0x01,                    // USAGE (Vendor Usage 1)
     0xa1, 0x01,                    // COLLECTION (Application)
-    0x15, 0x00,                    //   LOGICAL_MINIMUM (0)
-    0x26, 0xff, 0x00,              //   LOGICAL_MAXIMUM (255)
-    0x75, 0x08,                    //   REPORT_SIZE (8)
-    0x95, 0x08,                    //   REPORT_COUNT (8)
-    0x09, 0x00,                    //   USAGE (Undefined)
-    0xb2, 0x02, 0x01,              //   FEATURE (Data,Var,Abs,Buf)
+    0x75, 0x10,                    //   REPORT_SIZE (16)
+    0x95, 0x01,                    //   REPORT_COUNT (1)
+    0x81, 0x02,                    //   INPUT (Data,Var,Abs)
     0xc0                           // END_COLLECTION
 };
-/* Since we define only one feature report, we don't use report-IDs (which
- * would be the first byte of the report). The entire report consists of 16
- * opaque data bytes.
- */
-
-static int8_t currentState[8];
-
-void freqChange(uchar i, uchar c, uchar min, uchar max)
-{
-	uchar curr = currentState[i];
-	curr += c;
-	if(curr > max)
-	{
-		currentState[i] = max;
-	}
-	else if(curr < min)
-	{
-		currentState[i] = min;
-	}
-	else
-	{
-		currentState[i] = curr;
-	}
-}
-
-void dataRead(uchar *data)
-{
-	for(int i = 0; i < 8; i++)
-	{
-		data[i] = currentState[i];
-	}
-}
-
-void dataWrite(uchar *data)
-{
-for(int i = 0; i < 8; i++)
-	{
-		currentState[i] = data[i];
-	}
-}
-
-/* ------------------------------------------------------------------------- */
-
-/* usbFunctionRead() is called when the host requests a chunk of data from
- * the device. For more information see the documentation in usbdrv/usbdrv.h.
- */
-uchar   usbFunctionRead(uchar *data, uchar len)
-{
-	PORTB ^= 0x01;
-	dataRead(data);
-   	return len;
-}
-
-/* usbFunctionWrite() is called when the host sends a chunk of data to the
- * device. For more information see the documentation in usbdrv/usbdrv.h.
- */
-uchar   usbFunctionWrite(uchar *data, uchar len)
-{
-	dataWrite(data);
-	return 1;
-}
 
 /* ------------------------------------------------------------------------- */
 
 usbMsgLen_t usbFunctionSetup(uchar data[8])
 {
-usbRequest_t    *rq = (void *)data;
-    if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* HID class request */
-        if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+	usbRequest_t    *rq = (void *)data;
+
+	usbMsgPtr = reportBuffer;
+	if((rq->bmRequestType & USBRQ_TYPE_MASK) == USBRQ_TYPE_CLASS){    /* class request type */
+		if(rq->bRequest == USBRQ_HID_GET_REPORT){  /* wValue: ReportType (highbyte), ReportID (lowbyte) */
+			/* we only have one report type, so don't look at wValue */
+			curRadio->buildReport(reportBuffer);
+			return curRadio->report_size;
+		}else if(rq->bRequest == USBRQ_HID_SET_REPORT){
             /* since we have only one report type, we can ignore the report-ID */
-            return USB_NO_MSG;  /* use usbFunctionRead() to obtain data */
-        }else if(rq->bRequest == USBRQ_HID_SET_REPORT){
-            /* since we have only one report type, we can ignore the report-ID */
+            bytesRemaining = REPORT_SIZE;
+            currentAddress = 0;
             return USB_NO_MSG;  /* use usbFunctionWrite() to receive data from host */
         }
-    }else{
-        /* ignore vendor type requests, we don't use any */
-    }
-    return 0;
+	}else{
+	/* no vendor specific requests implemented */
+	}
+	return 0;
+
+/* ------------------------------------------------------------------------- */
+    
+static uchar    currentAddress;
+static uchar    bytesRemaining;
+    
+uchar   usbFunctionWrite(uchar *data, uchar len)
+{
+    if(bytesRemaining == 0)
+        return 1;               /* end of transfer */
+    if(len > bytesRemaining)
+        len = bytesRemaining;
+    memcpy(data last_read_radio_bytes[address], len)
+    memcpy(data last_reported_radio_bytes[address], len)
+    currentAddress += len;
+    bytesRemaining -= len;
+    return bytesRemaining == 0; /* return 1 if this was the last chunk */
 }
 
 /* ------------------------------------------------------------------------- */
 
-volatile int8_t enc_delta;			// -128 ... 127
-static int8_t last;
- 
- 
-void encode_init( void )
-{
-  int8_t new;
- 
-  new = 0;
-  if( PHASE_A )
-    new = 3;
-  if( PHASE_B )
-    new ^= 1;					// convert gray to binary
-  last = new;					// power on state
-  enc_delta = 0;
-  TCCR0 = 0<<CS02^1<<CS01^1<<CS00;		// CTC, XTAL / 64
-  OCR1B = (uint8_t)(XTAL / 64.0 * 1e-3 - 0.5);	// 1ms
-  TIMSK |= 1<<TOIE0;
-}
- 
- 
-ISR( SIG_OVERFLOW0 )				// 1ms for manual movement
-{
-  int8_t new, diff;
- 
-  new = 0;
-  if( PHASE_A )
-    new = 3;
-  if( PHASE_B )
-    new ^= 1;					// convert gray to binary
-  diff = last - new;				// difference last - new
-  if( diff & 1 ){				// bit 0 = value (1)
-    last = new;					// store new as next last
-    enc_delta += (diff & 2) - 1;		// bit 1 = direction (+/-)
-  }
-}
- 
- 
-int8_t encode_read1( void )			// read single step encoders
-{
-  int8_t val;
- 
-  cli();
-  val = enc_delta;
-  enc_delta = 0;
-  sei();
-  return val;					// counts since last call
-}
- 
- 
-int8_t encode_read2( void )			// read two step encoders
-{
-  int8_t val;
- 
-  cli();
-  val = enc_delta;
-  enc_delta = val & 1;
-  sei();
-  return val >> 1;
-}
- 
- 
-int8_t encode_read4( void )			// read four step encoders
-{
-  int8_t val;
- 
-  cli();
-  val = enc_delta;
-  enc_delta = val & 3;
-  sei();
-  return val >> 2;
-}
-
+static Radio *curRadio;
+    
 int main(void)
 {
-uchar   i;
+    uchar   i;
+    char must_report = 0, first_run = 1;
 
     wdt_enable(WDTO_1S);
     /* Even if you don't use the watchdog, turn it off here. On newer devices,
@@ -216,7 +101,9 @@ uchar   i;
      */
     odDebugInit();
     usbInit();
-	encode_init();
+	
+    curRadio = getRadio();
+    
     usbDeviceDisconnect();  /* enforce re-enumeration, do this while interrupts are disabled! */
     i = 0;
     while(--i){             /* fake USB disconnect for > 250 ms */
@@ -227,15 +114,6 @@ uchar   i;
     sei();
     DBG1(0x01, 0, 0);       /* debug output: main loop starts */
 
-	currentState[0] = 1;
-	currentState[1] = 120;
-	currentState[2] = 35;
-	currentState[3] = 129;
-	currentState[4] = 60;
-	currentState[5] = 0x0f;
-	currentState[6] = 0xf0;
-	currentState[7] = 0;
-
 	DDRB = 0x00;
 	DDRB ^= 0x01;
 	PORTB = 0xff;
@@ -244,7 +122,36 @@ uchar   i;
         DBG1(0x02, 0, 0);   /* debug output: main loop iterates */
         wdt_reset();
         usbPoll();
-		freqChange(1, encode_read2(), 118, 136);
+        
+        if(first_run) {
+            curRadio->update();
+            first_run = 0;
+        }
+        
+        if (TIFR & (1<<OCF2))
+		{
+			TIFR = 1<<OCF2;
+			if (!must_report)
+			{
+				curRadio->update();
+				if (curRadio->changed()) {
+					must_report = 1;
+				}
+			}
+		}
+		
+			
+		if(must_report)
+		{
+			if (usbInterruptIsReady())
+			{ 	
+
+			must_report = 0;
+
+			curRadio->buildReport(reportBuffer);
+			usbSetInterrupt(reportBuffer, curRadio->report_size);
+			}
+		}
     }
     return 0;
 }
--- firmware/usbconfig.h
+++ firmware/usbconfig.h
@@ -108,7 +108,7 @@ section at the end of this file).
 /* Define this to 1 if the device has its own power supply. Set it to 0 if the
  * device is powered from the USB bus.
  */
-#define USB_CFG_MAX_BUS_POWER           20
+#define USB_CFG_MAX_BUS_POWER           80
 /* Set this variable to the maximum USB bus power consumption of your device.
  * The value is in milliamperes. [It will be divided by two since USB
  * communicates power requirements in units of 2 mA.]
@@ -118,7 +118,7 @@ section at the end of this file).
  * transfers. Set it to 0 if you don't need it and want to save a couple of
  * bytes.
  */
-#define USB_CFG_IMPLEMENT_FN_READ       1
+#define USB_CFG_IMPLEMENT_FN_READ       0
 /* Set this to 1 if you need to send control replies which are generated
  * "on the fly" when usbFunctionRead() is called. If you only want to send
  * data from a static buffer, set it to 0 and return the data from
